ee882c0aaf21c10c11194f96a38706fba345f182
ajlopez <webmaster@ajlopez.com>
2019-09-27 08:03:37 -0300

    Swap with sender and receiver; proposal with proposer and executor

--- contracts/Swap.sol
+++ contracts/Swap.sol
@@ -5,47 +5,62 @@ contract Swap {
     
     struct SendData {
         address sender;
-        uint nonce;
+        address receiver;
         address token;
         uint amount;
+        uint nonce;
     }
     
     struct ProposalData {
         address proposer;
+        address executor;
         bytes32 sendID;
         address token;
         uint amount;
         bytes32 hash;
     }
     
+    struct ChallengeData {
+        bytes32 proposalID;
+        bytes32 hash;
+    }
+    
     mapping (bytes32 => SendData) public sends;
     mapping (bytes32 => ProposalData) public proposals;
+    mapping (bytes32 => ChallengeData[]) public challenges;
     
-    event Send(bytes32 indexed id, address indexed sender, uint nonce, address indexed token, uint amount);
-    event Proposal(bytes32 indexed id, bytes32 indexed sendID, address indexed proposer, address token, uint amount);
+    event Send(bytes32 indexed id, address indexed sender, address receiver, address indexed token, uint amount, uint nonce);
+    event Proposal(bytes32 indexed id, bytes32 indexed sendID, address indexed proposer, address executor, address token, uint amount);
+    event Challenge(bytes32 indexed sendID, bytes32 indexed proposalID, bytes32 hash);
     
-    function openSend(address token, uint amount) public returns (bytes32) {
+    function openSend(address receiver, address token, uint amount) public returns (bytes32) {
         require(amount > 0);
         
-        bytes32 id = keccak256(abi.encodePacked(msg.sender, nonce, token, amount));
+        bytes32 id = keccak256(abi.encodePacked(msg.sender, receiver, token, amount, nonce));
         
-        sends[id] = SendData(msg.sender, nonce, token, amount);
+        sends[id] = SendData(msg.sender, receiver, token, amount, nonce);
         
-        emit Send(id, msg.sender, nonce, token, amount);
+        emit Send(id, msg.sender, receiver, token, amount, nonce);
         
         nonce++;
         
         return id;
     }
     
-    function doProposal(bytes32 sendID, address token, uint amount) public returns (bytes32) {
+    function addChallenge(bytes32 sendID, bytes32 proposalID, bytes32 hash) public returns (bool) {
+        emit Challenge(sendID, proposalID, hash);
+        
+        return true;
+    }
+    
+    function doProposal(address executor, bytes32 sendID, address token, uint amount) public returns (bytes32) {
         require(amount > 0);
         
-        bytes32 id = keccak256(abi.encodePacked(msg.sender, sendID, token, amount));
+        bytes32 id = keccak256(abi.encodePacked(msg.sender, executor, sendID, token, amount));
         
-        proposals[id] = ProposalData(msg.sender, sendID, token, amount, 0);
+        proposals[id] = ProposalData(msg.sender, executor, sendID, token, amount, 0);
         
-        emit Proposal(id, sendID, msg.sender, token, amount);
+        emit Proposal(id, sendID, msg.sender, executor, token, amount);
         
         return id;
     }
--- test/Swap_test.js
+++ test/Swap_test.js
@@ -7,7 +7,9 @@ contract('Swap', function (accounts) {
     const owner = accounts[0];
     const alice = accounts[1];
     const bob = accounts[2];
-    const token = accounts[3];
+    const charlie = accounts[3];
+    const dan = accounts[4];
+    const token = accounts[5];
 
     beforeEach(async function() {
         this.swap = await Swap.new();
@@ -20,7 +22,7 @@ contract('Swap', function (accounts) {
     });
     
     it('open send', async function () {
-        const result = await this.swap.openSend(token, 1000, { from: alice });
+        const result = await this.swap.openSend(charlie, token, 1000, { from: alice });
         
         assert.ok(result);
         assert.ok(result.logs);
@@ -29,10 +31,11 @@ contract('Swap', function (accounts) {
         const log = result.logs[0];
         
         assert.equal(log.event, 'Send');
-        assert.equal(log.args.nonce, 0);
         assert.equal(log.args.token, token);
         assert.equal(log.args.amount, 1000);
         assert.equal(log.args.sender, alice);
+        assert.equal(log.args.receiver, charlie);
+        assert.equal(log.args.nonce, 0);
         
         const nonce = await this.swap.nonce();
         
@@ -41,13 +44,14 @@ contract('Swap', function (accounts) {
         const data = await this.swap.sends(log.args.id);
         
         assert.equal(data.sender, alice);
+        assert.equal(data.receiver, charlie);
         assert.equal(data.token, token);
         assert.equal(data.amount, 1000);
         assert.equal(data.nonce, 0);
     });
     
     it('cannot send zero value', async function () {
-        await expectThrow(this.swap.openSend(token, 0, { from: alice }));
+        await expectThrow(this.swap.openSend(charlie, token, 0, { from: alice }));
 
         const nonce = await this.swap.nonce();
         
@@ -55,7 +59,7 @@ contract('Swap', function (accounts) {
     });
     
     it('do proposal', async function () {
-        const result = await this.swap.doProposal('0x01', token, 1000, { from: bob });
+        const result = await this.swap.doProposal(dan, '0x01', token, 1000, { from: bob });
         
         assert.ok(result);
         assert.ok(result.logs);
@@ -66,6 +70,7 @@ contract('Swap', function (accounts) {
         assert.equal(log.event, 'Proposal');
         assert.equal(log.args.sendID, '0x0100000000000000000000000000000000000000000000000000000000000000');
         assert.equal(log.args.proposer, bob);
+        assert.equal(log.args.executor, dan);
         assert.equal(log.args.token, token);
         assert.equal(log.args.amount, 1000);
         
@@ -73,6 +78,7 @@ contract('Swap', function (accounts) {
         
         assert.equal(data.sendID, '0x0100000000000000000000000000000000000000000000000000000000000000');
         assert.equal(data.proposer, bob);
+        assert.equal(data.executor, dan);
         assert.equal(data.token, token);
         assert.equal(data.amount, 1000);
         assert.equal(data.hash, '0x0000000000000000000000000000000000000000000000000000000000000000');
