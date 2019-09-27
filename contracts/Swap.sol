f57d5a36b04331ce6ca7d7dd49f440716d065191
ajlopez <webmaster@ajlopez.com>
2019-09-27 13:21:40 -0300

    Removing challenge code

--- contracts/Swap.sol
+++ contracts/Swap.sol
@@ -20,18 +20,11 @@ contract Swap {
         bytes32 hash;
     }
     
-    struct ChallengeData {
-        bytes32 proposalID;
-        bytes32 hash;
-    }
-    
     mapping (bytes32 => SendData) public sends;
     mapping (bytes32 => ProposalData) public proposals;
-    mapping (bytes32 => ChallengeData[]) public challenges;
     
     event Send(bytes32 indexed id, address indexed sender, address receiver, address indexed token, uint amount, uint nonce);
     event Proposal(bytes32 indexed id, bytes32 indexed sendID, address indexed proposer, address executor, address token, uint amount);
-    event Challenge(bytes32 indexed sendID, bytes32 indexed proposalID, bytes32 hash);
     
     function openSend(address receiver, address token, uint amount) public returns (bytes32) {
         require(amount > 0);
@@ -45,12 +38,6 @@ contract Swap {
         nonce++;
         
         return id;
-    }
-    
-    function addChallenge(bytes32 sendID, bytes32 proposalID, bytes32 hash) public returns (bool) {
-        emit Challenge(sendID, proposalID, hash);
-        
-        return true;
     }
     
     function doProposal(address executor, bytes32 sendID, address token, uint amount) public returns (bytes32) {
